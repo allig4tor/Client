0c138750533e6e03b53fdb68708341fcf12101fd
Ryan <ryanmaddennew@gmail.com>
2021-11-02 14:31:28 -0400

    listen and display messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,22 @@ import { ChatService } from './chat.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
   message: string;
+  messages: string[] = [];
 
-  constructor( private chatService: ChatService) { }
+  constructor(private chatService: ChatService) {
+  }
 
   sendMessage() {
     this.chatService.sendMessage(this.message);
     this.message = '';
+  }
+
+  ngOnInit() {
+    this.chatService
+      .getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
   }
 }
--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,14 +1,32 @@
+import { BehaviorSubject, Observable } from 'rxjs';
 import * as io from 'socket.io-client';
+
+
+
 
 export class ChatService {
     private url = 'http://localhost:3000';
     private socket;
+    // private _messages$ = new BehaviorSubject<string>(null);
+    // get messages$(): Observable<string> {
+    //   return this._messages$.asObservable();
+    // }
 
     constructor() {
-        this.socket = io.connect(this.url, {transports: ['websocket']});
+      this.socket = io.connect(this.url, { transports: ['websocket'] });
     }
 
     public sendMessage(message) {
-      this.socket.emit('new-message', message);
+        this.socket.emit('new-message', message);
+    }
+
+    public getMessages = () => {
+      return Observable.create((observer) => {
+        console.log('huh?')
+          this.socket.on('new-message', (message) => {
+
+              observer.next(message);
+          });
+      });
   }
 }
